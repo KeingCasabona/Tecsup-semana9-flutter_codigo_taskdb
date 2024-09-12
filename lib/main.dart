0964ed7f703ab9627ff4392bc194f7793fb9b944
KeingCasabona <keing.limache.casabona@gmail.com>
2024-09-11 21:07:17 -0500

    feat: Structuring our project and ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo_taskdb/pages/home_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,15 +10,26 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
+      title: 'TaskaDBApp',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        secondaryHeaderColor: Color(0xff01c851),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.black,
+          //ICONOS POR ENCIMA DEL APPBAR:
+          foregroundColor: Colors.white,
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        scaffoldBackgroundColor: Colors.white,
+        // floatingActionButtonTheme: FloatingActionButtonThemeData(
+        //   backgroundColor: Color(0xff01c851),
+        // ),
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(
+            color: Colors.black,
+          ),
         ),
       ),
+      home: HomePage(),
     );
   }
 }
